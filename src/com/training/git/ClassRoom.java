492d08555d62fe614d2c375bc048fa1aa5ff4e35
Rajamohan Raju <rajraju@deloitte.com>
2019-11-10 23:12:45 +0530

    initial commit

---
+++ src/com/training/git/ClassRoom.java
@@ -0,0 +1,51 @@
+package com.training.git;
+
+public class ClassRoom {
+
+  private String meetingRoomName;
+
+  private String meetingRoomSeq;
+
+  private int floorNo;
+
+  public ClassRoom() {
+  }
+
+  public ClassRoom(String meetingRoomName, String meetingRoomSeq, int floorNo) {
+    super();
+    this.meetingRoomName = meetingRoomName;
+    this.meetingRoomSeq = meetingRoomSeq;
+    this.floorNo = floorNo;
+  }
+
+  public String getMeetingRoomName() {
+    return meetingRoomName;
+  }
+
+  public void setMeetingRoomName(String meetingRoomName) {
+    this.meetingRoomName = meetingRoomName;
+  }
+
+  public String getMeetingRoomSeq() {
+    return meetingRoomSeq;
+  }
+
+  public void setMeetingRoomSeq(String meetingRoomSeq) {
+    this.meetingRoomSeq = meetingRoomSeq;
+  }
+
+  public int getFloorNo() {
+    return floorNo;
+  }
+
+  public void setFloorNo(int floorNo) {
+    this.floorNo = floorNo;
+  }
+
+  @Override
+  public String toString() {
+    return "ClassRoom [meetingRoomName=" + meetingRoomName + ", meetingRoomSeq=" + meetingRoomSeq + ", floorNo="
+        + floorNo + "]";
+  }
+
+}
